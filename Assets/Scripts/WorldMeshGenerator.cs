b661f4036bb76a23a11d16aeac4662925328d4ee
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2022-11-09 18:04:56 +0400

    Fist vertex

--- Assets/Scripts/WorldMeshGenerator.cs
+++ Assets/Scripts/WorldMeshGenerator.cs
@@ -13,20 +13,39 @@ public class WorldMeshGenerator : MonoBehaviour
     private Mesh _mesh;
     private MeshCollider _col;
 
-    private int _faceCount;
 
-    float width;
-    float length;
+    float _width;
+    float _length;
 
     void Start(){
         _mesh = GetComponent<MeshFilter> ().mesh;
         _col = GetComponent<MeshCollider> ();
 
-        width = transform.
+        _width = transform.localScale.x;
+        _length = transform.localScale.z;
+
+
+        _FieldVertices.Add(new Vector3(0,0,0));
+        _FieldVertices.Add(new Vector3(0,0,_length));
+        _FieldVertices.Add(new Vector3(_width,0,_length));
+        _FieldVertices.Add(new Vector3(_width,0,0));
+
+
+        _FieldTriangles.Add(0);
+        _FieldTriangles.Add(1);
+        _FieldTriangles.Add(2);
+        _FieldTriangles.Add(0);
+        _FieldTriangles.Add(2);
+        _FieldTriangles.Add(3);
+
+        UpdateMesh();
     }
 
-    void Update()
-    {
-        
+    void UpdateMesh(){
+        _mesh.Clear();
+        _mesh.vertices = _FieldVertices.ToArray();
+        _mesh.triangles = _FieldTriangles.ToArray();
+        _mesh.Optimize();
+        _mesh.RecalculateNormals();
     }
 }
