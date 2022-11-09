5f7ca1a3dc82d7736868377da83303b9b931c44e
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2022-11-09 18:40:42 +0400

    path generation basic

--- Assets/Scripts/WorldMeshGenerator.cs
+++ Assets/Scripts/WorldMeshGenerator.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(MeshCollider))]
 public class WorldMeshGenerator : MonoBehaviour
 {
-    
+    [SerializeField] float _roadThickness = 2;
+    [SerializeField] float _width = 1;
+    [SerializeField] float _length = 1;
      
     private List<Vector3> _FieldVertices = new List<Vector3>();
     private List<int> _FieldTriangles = new List<int>();
@@ -13,16 +15,14 @@ public class WorldMeshGenerator : MonoBehaviour
     private Mesh _mesh;
     private MeshCollider _col;
 
-
-    float _width;
-    float _length;
-
+    int _faceCount;
+  
     void Start(){
         _mesh = GetComponent<MeshFilter> ().mesh;
         _col = GetComponent<MeshCollider> ();
-
         _width = transform.localScale.x;
         _length = transform.localScale.z;
+        _faceCount = 0;
 
 
         _FieldVertices.Add(new Vector3(0,0,0));
@@ -34,9 +34,56 @@ public class WorldMeshGenerator : MonoBehaviour
         _FieldTriangles.Add(0);
         _FieldTriangles.Add(1);
         _FieldTriangles.Add(2);
+
         _FieldTriangles.Add(0);
         _FieldTriangles.Add(2);
         _FieldTriangles.Add(3);
+
+        float widthSqr = _roadThickness * _roadThickness; // 4
+        float distanceSqr = widthSqr / 2f; //2
+        float distance = Mathf.Sqrt(distanceSqr); //1.14
+
+        _FieldVertices.Add(new Vector3(distance,            0, distance ));
+        _FieldVertices.Add(new Vector3(distance,            0, _length - distance ));
+        _FieldVertices.Add(new Vector3(_width - distance,   0, _length - distance ));
+        _FieldVertices.Add(new Vector3(_width - distance, 0, distance));
+        //Left
+        _FieldTriangles.Add(0);
+        _FieldTriangles.Add(1);
+        _FieldTriangles.Add(5);
+
+        _FieldTriangles.Add(5);
+        _FieldTriangles.Add(4);
+        _FieldTriangles.Add(0);
+
+        //Top
+        _FieldTriangles.Add(1);
+        _FieldTriangles.Add(2);
+        _FieldTriangles.Add(6);
+
+        _FieldTriangles.Add(6);
+        _FieldTriangles.Add(5);
+        _FieldTriangles.Add(1);
+
+        //Right
+        _FieldTriangles.Add(2);
+        _FieldTriangles.Add(3);
+        _FieldTriangles.Add(7);
+
+        _FieldTriangles.Add(7);
+        _FieldTriangles.Add(6);
+        _FieldTriangles.Add(2);
+
+        //bot
+        _FieldTriangles.Add(3);
+        _FieldTriangles.Add(0);
+        _FieldTriangles.Add(4);
+
+        _FieldTriangles.Add(4);
+        _FieldTriangles.Add(7);
+        _FieldTriangles.Add(3);
+
+
 
         UpdateMesh();
     }
